686c689b9b5406cc23a682d1e04da48769f1e629
lllSlll <sunny90224@gmail.com>
2022-11-02 14:43:17 +0900

    Authentication

--- app/src/main/java/edu/sns/memorystack/LoginActivity.kt
+++ app/src/main/java/edu/sns/memorystack/LoginActivity.kt
@@ -2,6 +2,8 @@ package edu.sns.memorystack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import edu.sns.memorystack.databinding.LoginLayoutBinding
 
 class LoginActivity : AppCompatActivity()
@@ -13,5 +15,33 @@ class LoginActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        if(binding.autoLogin.isChecked) {
+            //auto login
+        }
+
+        binding.loginButton.setOnClickListener loginButton@ {
+            val nickname = binding.nickname.text.toString()
+            val password = binding.password.text.toString()
+
+            if(nickname.isEmpty()) {
+                binding.nickname.error = "Please write Nickname"
+                return@loginButton
+            }
+            if(password.isEmpty()) {
+                binding.password.error = "Please write Password"
+                return@loginButton
+            }
+
+            Firebase.auth.signInWithEmailAndPassword(nickname, password)
+                .addOnCompleteListener {
+                    if(it.isSuccessful) {
+                        //login success
+                    }
+                    else {
+                        //login failed
+                    }
+                }
+        }
     }
 }
