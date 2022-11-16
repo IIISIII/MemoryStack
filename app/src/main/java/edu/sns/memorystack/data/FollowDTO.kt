243c94b3def685b43524d45972fbc08f1bc64ac2
cyujin <113533845+cyujin@users.noreply.github.com>
2022-11-16 18:21:49 +0900

    Follow기능 미완성, 팔로잉 수 가져오기
    edit버튼에 일단 구현

--- app/src/main/java/edu/sns/memorystack/data/FollowDTO.kt
+++ app/src/main/java/edu/sns/memorystack/data/FollowDTO.kt
@@ -1,9 +1,9 @@
 package edu.sns.memorystack.data
 
 data class FollowDTO(
-    var followerCount : Int = 1,
+    var followerCount : Int = 0,
     var followers : MutableMap<String,Boolean> = HashMap(),
 
-    var followingCount : Int = 1,
+    var followingCount : Int = 0,
     var followings : MutableMap<String,Boolean> = HashMap()
 )
--- app/src/main/java/edu/sns/memorystack/fragment/ProfileFragment.kt
+++ app/src/main/java/edu/sns/memorystack/fragment/ProfileFragment.kt
@@ -9,13 +9,13 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.commit
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import edu.sns.memorystack.EditProfileActivity
 import edu.sns.memorystack.R
+import edu.sns.memorystack.data.FollowDTO
 import edu.sns.memorystack.data.ProfileRepository
 import edu.sns.memorystack.method.AccountMethod
 import kotlinx.coroutines.CoroutineScope
@@ -24,24 +24,28 @@ import kotlinx.coroutines.launch
 
 class ProfileFragment: Fragment()
 {
+    var db :  FirebaseFirestore = Firebase.firestore
+    var auth = Firebase.auth
+    var currentUser = auth.currentUser
+    var uid = currentUser?.uid
 
     //
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
         return inflater.inflate(R.layout.profile_page, container, false)
-
     }
     //
     override fun onViewCreated(view: View, savedInstanceState: Bundle?)
     {
         super.onViewCreated(view, savedInstanceState)
+
         //파이어베이스 auth
-        val auth = Firebase.auth
-        val currentUser = auth.currentUser ?: return
-        val uid = currentUser?.uid
+        //val auth = Firebase.auth
+        //val currentUser = auth.currentUser ?: return
+        //val uid = currentUser?.uid
         println("####################${uid}")
         //파이어베이스 database
-        val db :  FirebaseFirestore = Firebase.firestore
+        //val db :  FirebaseFirestore = Firebase.firestore
         val itemsCollectionRef = db.collection("users")
         //nickname 설정
         val nickname = view.findViewById<TextView>(R.id.nickname)
@@ -62,11 +66,60 @@ class ProfileFragment: Fragment()
         val edit = view.findViewById<Button>(R.id.edit_profile)
 
         edit.setOnClickListener {
-           val intent = Intent(activity, EditProfileActivity::class.java)
-            startActivity(intent)
+//           val intent = Intent(activity, EditProfileActivity::class.java)
+//            startActivity(intent)
+
+            //팔로우 기능 확인차 넣어둠
+            doFollow()
             println("button clicked#############################")
+        }
+        getFollowerAndFollowing()
+    }
+
+    //follow
+    fun doFollow() {
+        // document(uid) -> 팔로잉 기능함
+        val tsDocFollowing = db.collection("follow").document(uid!!.toString())
+        db.runTransaction { transaction ->
+            var followDTO = transaction.get(tsDocFollowing).toObject(FollowDTO::class.java)
+            //팔로우 정보 없음 / 생성
+            if(followDTO == null){
+                followDTO = FollowDTO()
+                followDTO.followingCount = 1
+                followDTO.followings[uid!!] = true
+
+                transaction.set(tsDocFollowing,followDTO)
+                return@runTransaction
+            }
+            //팔로우 하고 있는 경우 / 취소
+            if(followDTO.followings.containsKey(uid)){
+                followDTO.followingCount = followDTO.followingCount - 1
+                followDTO.followings.remove(uid)
+            }
+            //팔로우 하고 있지 않은 경우 / 팔로우
+            else {
+                followDTO.followingCount = followDTO.followingCount + 1
+                followDTO.followings.set(uid!!, true)
+            }
+            transaction.set(tsDocFollowing,followDTO)
+            return@runTransaction
+        }
+        //내가 팔로우한 사람의 팔로워수
+
+    }
+
+    //팔로워, 팔로잉 수 받아옴
+    fun getFollowerAndFollowing(){
+        db.collection("follow").document(uid!!).addSnapshotListener { value, error ->
+            if(value == null) return@addSnapshotListener
+            val followDTO = value.toObject(FollowDTO::class.java)
+            if(followDTO?.followingCount != null){
+                view?.findViewById<TextView>(R.id.followingCount)?.text = followDTO.followingCount.toString()
+            }
+            if(followDTO?.followerCount != null) {
+                view?.findViewById<TextView>(R.id.followerCount)?.text =
+                    followDTO.followerCount.toString()
+            }
         }
     }
 }
-
-
